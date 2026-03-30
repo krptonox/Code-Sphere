8d085d0a8e20bfaa4e4a250e3fd78fa03147ab17
krptonox <aryanyadav182007@gmail.com>
2026-03-30 19:06:49 +0530

    utility setup for error , response, asynchandler

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,23 @@
 import express from 'express'
 const app = express()  //app has all functionality of express
 
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN, // Allow requests from the specified origin (e.g., http://localhost:5173)
+    }
+))
+
+app.use(express.json())//express.json() is a built-in middleware function in Express.js that parses incoming JSON requests and puts the parsed data in req.body. This allows you to easily access the data sent in the request body when handling routes that expect JSON input.
+
+app.use(express.urlencoded({extended:true}))//express.urlencoded() is a built-in middleware function in Express.js that parses incoming requests with URL-encoded payloads (such as form submissions) and puts the parsed data in req.body. The extended: true option allows for rich objects and arrays to be encoded into the URL-encoded format, which can be useful for handling complex form data.
+
+app.use(express.static('public'))//express.static() is a built-in middleware function in Express.js that serves static files from a specified directory. In this case, it serves files from the 'public' directory. This means that any files placed in the 'public' folder can be accessed directly via the URL, allowing you to serve images, CSS files, JavaScript files, and other static assets to the client without needing to define specific routes for each file.
+
+app.use(cookieParser())//cookieParser() is a middleware function that parses cookies attached to the client request object. It populates the req.cookies object with the parsed cookies, allowing you to easily access and manipulate cookie data in your route handlers. This is particularly useful for handling user sessions, authentication, and other features that rely on cookies for storing information on the client side.
+
 
 app.get('/', (req, res) => {  //app.get() is used to handle GET requests to the specified route ('/'). It takes a callback function with two parameters: req (the request object) and res (the response object). In this case, when a GET request is made to the root route ('/'), the server responds with 'Code Arena'.
     res.send('Code Arena')
---
+++ Backend/src/Utils/asyncHandler.js
@@ -0,0 +1,25 @@
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try{
+//       await fn(req,res,next)
+//     } catch(err){
+//         res.status(err.code || 500).json({message:"Internal Server Error"})
+//     }
+// }
+
+import { request } from "express";
+
+//this is try catch block for handling errors in asynchronous functions. It is a common pattern used in Express.js applications to catch and handle errors that may occur during the execution of asynchronous route handlers or middleware functions.
+
+
+//We will do using Promise and .then() and .catch() instead of async await and try catch block because it is more cleaner and easier to read.
+
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+//Use of asyncHandler?
+//The asyncHandler function is a higher-order function that takes an asynchronous request handler as an argument and returns a new function that wraps the original handler in a Promise. This allows you to handle errors that may occur during the execution of the asynchronous handler without needing to use try-catch blocks in each individual route handler. Instead, any errors that occur will be automatically caught and passed to the next middleware function (usually an error-handling middleware) for centralized error handling. This helps to keep your code cleaner and more maintainable by reducing repetitive error-handling code across your route handlers.
+
+export default asyncHandler;

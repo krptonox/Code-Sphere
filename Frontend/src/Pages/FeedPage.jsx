16aaa75cc8121b0e6614802bc0afd5ce0b3a5a8a
krptonox <aryanyadav182007@gmail.com>
2026-04-13 14:23:47 +0530

    Refactor FeedPage component: simplify class names for borders and improve layout consistency

--- Frontend/src/Pages/FeedPage.jsx
+++ Frontend/src/Pages/FeedPage.jsx
@@ -11,8 +11,8 @@ function FeedPage() {
 
   return (
     <div className='h-screen w-screen bg-gray-900 flex items-center justify-center'>
-      <div className='h-screen w-[15%] flex flex-col items-center justify-baseline bg-gray-800 border-r-[1px] border-gray-600 m-0'>
-        <h1 className='font-Segoe-UI text-white bg-gray-700 w-full h-[8%] flex items-center justify-center text-2xl border-b-[1px] border-gray-600 mb-[5%]'>CodeSphere.</h1>
+      <div className='h-screen w-[15%] flex flex-col items-center justify-baseline bg-gray-800 border-r border-gray-600 m-0'>
+        <h1 className='font-Segoe-UI text-white bg-gray-700 w-full h-[8%] flex items-center justify-center text-2xl border-b border-gray-600 mb-[5%]'>CodeSphere.</h1>
         <div className='w-full h-[50%] flex flex-col items-center justify-start gap-4'>
           <button className="w-[70%] h-[12%] hover:ring-[1px] text-gray-300 pl-[5%] focus:bg-gray-600 rounded-[5px] transition-all ease-[0.3s] hover:bg-gray-500 hover:text-white focus:border-l-[6px] focus:text-white  hover:shadow-green-600/60 border-green-600 font-google-sans-font-300 flex flex-col text-left justify-center">Dashboard</button>
           <button className="w-[70%] h-[12%] hover:ring-[1px] text-gray-300 pl-[5%] focus:bg-gray-600 rounded-[5px] transition-all ease-[0.3s] hover:bg-gray-500 hover:text-white focus:border-l-[6px] focus:text-white  hover:shadow-green-600/60 border-green-600 font-google-sans-font-300 flex flex-col text-left justify-center">Friends</button>
@@ -20,8 +20,8 @@ function FeedPage() {
           <button className="w-[70%] h-[12%] hover:ring-[1px] text-gray-300 pl-[5%] focus:bg-gray-600 rounded-[5px] transition-all ease-[0.3s] hover:bg-gray-500 hover:text-white focus:border-l-[6px] focus:text-white  hover:shadow-green-600/60 border-green-600 font-google-sans-font-300 flex flex-col text-left justify-center">Leaderboard</button>
           <button className="w-[70%] h-[12%] hover:ring-[1px] text-gray-300 pl-[5%] focus:bg-gray-600 rounded-[5px] transition-all ease-[0.3s] hover:bg-gray-500 hover:text-white focus:border-l-[6px] focus:text-white  hover:shadow-green-600/60 border-green-600 font-google-sans-font-300 flex flex-col text-left justify-center">Practice</button>
           <button className="w-[70%] h-[12%] hover:ring-[1px] text-gray-300 pl-[5%] focus:bg-gray-600 rounded-[5px] transition-all ease-[0.3s] hover:bg-gray-500 hover:text-white focus:border-l-[6px] focus:text-white  hover:shadow-green-600/60 border-green-600 font-google-sans-font-300 flex flex-col text-left justify-center">Integration</button>
-          <div className='h-[1px] w-[90%] bg-gray-400'></div>
-          <button className="w-[70%] h-[12%] text-gray-300 pl-[5%] focus:bg-gray-600 rounded-[5px] transition-all ease-[0.3s] hover:bg-gray-500 hover:text-white focus:border-l-[8px] focus:text-white border-green-600 font-google-sans-font-300 flex flex-col text-left justify-center">Settings</button>
+          <div className='h-px w-[90%] bg-gray-400'></div>
+          <button className="w-[70%] h-[12%] text-gray-300 pl-[5%] focus:bg-gray-600 rounded-[5px] transition-all ease-[0.3s] hover:bg-gray-500 hover:text-white focus:border-l-8 focus:text-white border-green-600 font-google-sans-font-300 flex flex-col text-left justify-center">Settings</button>
         </div>
       </div>
       <div className='h-screen w-[85%] flex flex-col items-center justify-center'></div>
